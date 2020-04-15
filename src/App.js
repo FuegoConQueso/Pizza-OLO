fe0e2457437d40c3ef9ccca7100ef1d7d6951771
John Arachtingi <fuegoconqueso@yahoo.com>
2020-04-15 15:21:00 -0500

    Added Cart

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Header text="Welcome to Joey's Pizzaria!" />
       </header>
 
-      <div> 
+      <div>
         <HomeBody text="Order online, and have a hot pizza
                         waiting for you in 30 minutes or less!" />
       </div>
