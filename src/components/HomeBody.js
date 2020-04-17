00f1a739b89a62599bf46c503b44b29d542ba911
marioQC16 <63430849+marioQC16@users.noreply.github.com>
2020-04-16 21:48:33 -0500

    Custom, Info, and Text changes
    Added a clickable custom pizza image.
    
    Added the info section including: Hours, Location and Number (Both location and number are fake).
    
    Adjusted the appearence of Text

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './HomeBody.css';
 import SpecialtyImage from '../assets/specialty-pizzas.jpg';
+import CustomImage from '../assets/custom-pizzas.jpg';
  
 class HomeBody extends Component {
   render() {
@@ -12,18 +13,42 @@ class HomeBody extends Component {
             <br />
             <div>
                 <div className="specialty">
-                    Specialty Pizza's
+                    Specialty Pizzas
                     <br />
                     <a href="/custom">
                         <img className="imgContainer" src={SpecialtyImage}/>
                     </a>
                 </div>
                 <div className="custom">
-                    Custom Pizza's
+                    Custom Pizzas
+                    <br />
+                    <a href="/custom">
+                        <img className="imgContainer" src={CustomImage}/>
+                    </a>
                 </div>
                 <div className="store-info">
                     Info
-                </div>
+                    <br /><br />
+
+                    <div className="store-hours">
+                        Hours
+                        <br />
+                        Monday-Friday: 11AM - 9PM
+                        <br />
+                        Saturday: 11AM - 9PM
+                        <br />
+                        Sunday: 11AM - 7PM
+                    </div>
+                    <br />
+                    <div className="store-contact">
+                        Contact
+                        <br />
+                        Phone: (202)-555-0133
+                        <br />
+                        Address: 952 Snow Grove, MN
+                    </div>
+
+                 </div>
 
             </div>
         </div>
